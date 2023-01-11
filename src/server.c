3205e5b406f426bf246024b4f3c2511951c7c99d
Tommy <47206288+tommysr@users.noreply.github.com>
2023-01-11 12:19:58 +0000

    server error handled

--- src/server.c
+++ src/server.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
@@ -7,28 +6,32 @@
 #include <string.h>
 #include <errno.h>
 #include <signal.h>
+
 #include "common.h"
 
-void sigint_handler();
+void sigint_handler(int sig);
 int queue_id;
 
 int main(int argc, char **argv)
 {
   key_t queue_key = create_key(2115);
   struct Message message_buff;
-
-  queue_id = create_msg_queue(queue_key);
   signal(SIGINT, sigint_handler);
+  queue_id = create_msg_queue(queue_key);
 
   while (1)
   {
-    if (msgrcv(queue_id, (struct Message *)&message_buff, sizeof(struct TextWithSource), SERVER, 0) == -1)
+    printf("[S] Waiting for messages\n");
+
+    message_buff.m_destination = SERVER;
+
+    if (msgrcv(queue_id, (struct Message *)&message_buff, sizeof(struct TextWithSource), message_buff.m_destination, 0) == -1)
     {
-      perror("failed to receive message\n");
-      exit(EXIT_FAILURE);
+      perror("[S] failed to receive message\n");
+      clear_abort();
     }
 
-    printf("\tReceived from %ld: %s -> %ld \n", message_buff.m_text_with_source.source, message_buff.m_text_with_source.text, message_buff.m_destination);
+    printf("[S] Received: %s [%ld: -> %ld]\n", message_buff.m_text_with_source.text, message_buff.m_text_with_source.source, message_buff.m_destination);
 
     size_t message_size = strlen(message_buff.m_text_with_source.text);
 
@@ -38,17 +41,45 @@ int main(int argc, char **argv)
     message_buff.m_destination = message_buff.m_text_with_source.source;
     message_buff.m_text_with_source.source = SERVER;
 
-    if (msgsnd(queue_id, (struct Message *)&message_buff, sizeof(struct TextWithSource), 0) == -1)
+    printf("[S] Sending: %s [%ld: -> %ld]\n", message_buff.m_text_with_source.text, message_buff.m_text_with_source.source, message_buff.m_destination);
+
+    int sending = 1;
+
+    do
     {
-      printf("cant send back processed message \n");
-      exit(EXIT_FAILURE);
-    }
+      if (msgsnd(queue_id, (struct Message *)&message_buff, sizeof(struct TextWithSource), IPC_NOWAIT) == -1)
+      {
+        if (errno == EAGAIN)
+        {
+          printf("[S] Message queue is full, retrying send operation in 3 seconds\n");
+          sleep(3);
+        }
+        else
+        {
+          perror("[S] Error in sending message to the queue\n");
+          clear_abort();
+        }
+      }
+      else
+      {
+        sending = 0;
+      }
+    } while (sending);
   }
 
+  remove_msg_queue(queue_id);
   exit(EXIT_SUCCESS);
 }
 
-void sigint_handler()
+void sigint_handler(int sig)
+{
+  printf("Signal SIGINT!\n");
+  remove_msg_queue(queue_id);
+  return;
+}
+
+void clear_abort()
 {
   remove_msg_queue(queue_id);
+  exit(EXIT_FAILURE);
 }
